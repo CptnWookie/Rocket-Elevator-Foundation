6ee182fc24a377a741845a8d66225662ffa5c26f
alex <alexandreleblanc89@gmail.com>
2020-11-25 00:14:03 -0500

    full_address method in Address model not found by address_spec

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 require 'address'
 
 describe Address do
+    describe '#full_address' do
+        context "Return City's Address" do
+            it 'returns string showing the current city in the address' do
+                expect(Address::full_address("city"))
+            end
+        end
+    end 
 end
